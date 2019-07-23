c2e41bceaf0bde9b46088bf012c3b1c3280a0ec2
Jojo Tutor <jeshrave003@gmail.com>
2019-07-23 23:20:49 +0800

    max instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'build/server.js',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      instances: '1',
+      instances: 'max',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
